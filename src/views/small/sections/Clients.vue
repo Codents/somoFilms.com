714266439266edbb2456175734489ae06a16db43
Manuel Alfaro Sierra <dokoto.moloko@gmail.com>
2018-07-04 19:52:56 +0200

    Clients proposal

--- src/views/small/sections/Clients.vue
+++ src/views/small/sections/Clients.vue
@@ -25,55 +25,94 @@ const Bodies = Matter.Bodies;
 let runner = null;
 let engine = null;
 let render = null;
+let world = null;
+let stack = null;
 
 export default {
   mounted() {
-    const height = screen.availHeight;
-    engine = Engine.create();
-    const world = engine.world;
-    render = Render.create({
-      element: this.$refs.world,
-      engine: engine,
-      options: {
-        width: screen.availWidth,
-        height,
-        showAngleIndicator: true
-      }
-    });
-    const wallsWidth = 10;
-    World.add(world, [
-      // walls : Se calcula por su centro
-      Bodies.rectangle(screen.availWidth / 2, 0, screen.availWidth, wallsWidth, { isStatic: true }), // Top
-      Bodies.rectangle(screen.availWidth / 2, height, screen.availWidth, wallsWidth, { isStatic: true }), // Bottom
-      Bodies.rectangle(screen.availWidth, height / 2, wallsWidth, height, { isStatic: true }), // Right
-      Bodies.rectangle(0, height / 2, wallsWidth, height, { isStatic: true }) // Left
-    ]);
-    runner = Runner.create();
-    const stack = this.clientBuilder();
-    World.add(world, stack);
-    const mouse = Mouse.create(render.canvas);
-    const mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                    visible: false
-                }
-            }
-        });
-
-    World.add(world, mouseConstraint);
-    Render.lookAt(render, {
-      min: { x: 0, y: 0 },
-      max: { x: screen.availWidth, y: height }
-    });
+    console.log('CLIENTS MOUNTED');
   },
   methods: {
     handle() {
       console.log('Init animation');
-      Matter.Render.stop(render);
+      this.destroyWorld();
+      this.initWorld();
+      this.compoundWorld();
+    },
+    initWorld() {
+      engine = Engine.create();
+      world = engine.world;
+      render = Render.create({
+        element: this.$refs.world,
+        engine: engine,
+        options: {
+          width: window.innerWidth,
+          height: window.innerHeight,
+          showAngleIndicator: true
+        }
+      });
+      runner = Runner.create();
+      Render.lookAt(render, {
+        min: { x: 0, y: 0 },
+        max: { x: window.innerWidth, y: window.innerHeight }
+      });
+    },
+    compoundWorld() {
+      if (!stack) stack = this.clientBuilder();
+      World.add(world, stack);
+      World.add(world, this.contentBuilder());
+      World.add(world, this.createMouseConstrant());
       Render.run(render);
       Runner.run(runner, engine);
+    },
+    destroyWorld() {
+      if (render) {
+        Matter.Render.stop(render);
+        render.canvas.remove();
+        render.canvas = null;
+        render.context = null;
+        render.textures = {};
+      }
+      if (world) Matter.World.clear(world);
+      if (engine) Matter.Engine.clear(engine);
+    },
+    contentBuilder() {
+      const height = window.innerHeight;
+      const width = window.innerWidth;
+      const wallsWidth = 10;
+      return [
+        // walls : Se calcula por su centro
+        Bodies.rectangle(
+          width / 2,
+          0,
+          width,
+          wallsWidth,
+          { isStatic: true }
+        ), // Top
+        Bodies.rectangle(
+          width / 2,
+          height,
+          width,
+          wallsWidth,
+          { isStatic: true }
+        ), // Bottom
+        Bodies.rectangle(width, height / 2, wallsWidth, height, {
+          isStatic: true
+        }), // Right
+        Bodies.rectangle(0, height / 2, wallsWidth, height, { isStatic: true }) // Left
+      ];
+    },
+    createMouseConstrant() {
+      const mouse = Mouse.create(render.canvas);
+      return MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false
+          }
+        }
+      });
     },
     clientBuilder() {
       return Composites.stack(20, 20, 10, 5, 0, 0, function(x, y) {
@@ -130,6 +169,7 @@ export default {
   flex-shrink: 0;
   overflow-y: auto;
   .t-bottom {
+    position: absolute;
     height: 20px;
     flex-shrink: 0;
     width: 100px;
